79b72ad06a6006fb02e2d9bc7414dc976035b230
mollytaryn <mollytaryn@gmail.com>
2015-06-04 13:45:46 -0500

    setup spec features

--- spec/features/student_records_attendance_spec.rb
+++
@@ -1,6 +0,0 @@
-feature "Student records attendance" do
-
-  scenario "happy path, recording today's attendance" do
-    
-  end
-end
---
+++ spec/features/user_records_attendances_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.feature "UserRecordsAttendances", type: :feature do
+  pending "add some scenarios (or delete) #{__FILE__}"
+end
